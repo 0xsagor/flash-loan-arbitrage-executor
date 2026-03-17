1e89d57a337e328eb5ecb9b9fb40bb238dbe7ccf
Sagor <161376518+0xsagor@users.noreply.github.com>
2026-03-17 22:22:50 +0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+// Aave V3 Pool Addresses Provider (Example: Polygon Mainnet)
+const POOL_ADDRESSES_PROVIDER = "0xa97684ead0e451d9865913414a57b03111050a64";
+
+// Token Addresses
+const USDC = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174";
+const WMATIC = "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270";
+
+module.exports = {
+    POOL_ADDRESSES_PROVIDER,
+    USDC,
+    WMATIC
+};
